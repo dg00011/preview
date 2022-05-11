646ffe3b4abee567c925338d2d5394a110953d49
Dheeraj Gupta <dheerajg@digitizeco.in>
2022-05-11 15:35:26 +0530

    initail

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [src, setsrc] = useState();
+
+  const displayIMG = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const url = URL.createObjectURL(event.target.files[0]);
+      setsrc(url);
+      console.log("show", URL.createObjectURL(event.target.files[0]));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        accept=".png , .jpg , .jpeg"
+        name="pic"
+        id="pic"
+        onChange={(e) => displayIMG(e)}
+      />
+
+      <img src={src} alt="..." className="pic" />
     </div>
   );
 }
